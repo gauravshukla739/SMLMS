ce4b447f94b6e1f07982e821e7336511b307f08a
Gaurav shukla <gauravshukla@sourcemash.com>
2020-03-19 19:23:00 +0530

    Add project files.

---
+++ SMLMS.Model/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SMLMS.Model
+{
+    public class Class1
+    {
+    }
+}
---
+++ SMLMS.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SMLMS.Services
+{
+    public class Class1
+    {
+    }
+}
