dd96b0b6d14baa78ef1e79f2ad3ab3fae6797267
gorabraturi@sourcemash.com <gorabraturi@sourcemash.com>
2020-03-27 20:00:56 +0530

    Added Sql query

---
+++ SMLMS.Model/Core/LeaveRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SMLMS.Model.Core
+{
+    public class LeaveRequest : BaseEntity
+    {
+        public int Id { get; set; }
+        public string FromDate { get; set; }
+        public string ToDate { get; set; }
+        public string DayBegin { get; set; }
+        public string DayEnd { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int ShortLeaveFrom { get; set; }
+        public int ShortLeaveTo { get; set; }
+        public string Reason { get; set; }
+        public string RoleId { get; set; }
+
+    }
+}
---
+++ SMLMS.REST/script/HRM_Table.sql
@@ -0,0 +1,31 @@
+﻿create table LeaveType(
+Id int primary key Identity(1,1),
+Name varchar(50),
+Count int,
+CreateDate datetime default getdate(),
+UpdateDate datetime,
+CreatedBy varchar(50),
+UpdatedBy varchar(50),
+IsDeleted bit
+                     
+)
+
+
+create table LeaveRequest(
+Id int primary key Identity(1,1),
+FromDate date,
+ToDate date,
+DayBegin varchar(15),
+DayEnd varchar(15),
+LeaveTypeId int foreign key references LeaveType(Id),
+ShortLeaveFrom int,
+ShortLeaveTo int,
+Reason varchar(100),
+RoleId nvarchar(450) foreign key references AspNetRoles(Id),
+CreateDate datetime default getdate(),
+UpdateDate datetime,
+CreatedBy varchar(50),
+UpdatedBy varchar(50),
+IsDeleted bit
+                     
+)
