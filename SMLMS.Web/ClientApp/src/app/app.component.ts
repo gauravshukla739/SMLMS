eea65db2275bea514d63326807157f12d6b89bc1
Gaurav shukla <gauravshukla@sourcemash.com>
2020-03-31 21:30:10 +0530

    final code for the angualr app

--- SMLMS.Web/ClientApp/src/app/app.component.ts
+++ SMLMS.Web/ClientApp/src/app/app.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { SharedService } from './shared/services/shared.service.';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit, OnDestroy{
+  showPupup: boolean = false;
+  showLoader: boolean = false;
+  title = 'SM Portal';
+  popUpMsg: string;
+  popupSubscription: Subscription;
+  loaderSubscription: Subscription;
+
+  constructor(private service: SharedService) {
+  }
+
+  ngOnInit() {
+    this.popupSubscription = this.service.popupStatus.subscribe((value: any) => {
+      this.popUpMsg = value;
+      this.showPupup = true;
+      setTimeout(() => {
+        this.showPupup = false;
+      }, 2500);
+    });
+
+    this.loaderSubscription = this.service.loadingStatus.subscribe((value: any) => {
+      this.showLoader = value;
+    });
+  }
+  dismiss() {
+    this.showPupup = false;
+  }
+  ngOnDestroy() {
+    this.popupSubscription.unsubscribe();
+  }
 }
